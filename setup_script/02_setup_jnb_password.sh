81c8185e130f63755c4db2d3c729fec9c2cd9b53
seitaro <seitaro.hrkw@gmail.com>
2017-12-05 02:20:42 +0900

    setup_scriptから不要なechoを削除した

--- setup_script/02_setup_jnb_password.sh
+++ setup_script/02_setup_jnb_password.sh
@@ -6,10 +6,5 @@ python -c 'from notebook.auth import passwd;print(passwd())'
 echo "↑をコピーして"
 echo "~/.jupyter/jupyter_notebook_config.pyに"
 echo "こんなかんじに貼り付けてください"
-echo "c.NotebookApp.password = 'sha1:〜〜〜〜〜〜'"
 
-echo "Homeフォルダの表示方法は"
-echo "c9の歯車アイコンをクリックして"
-echo "Show Hoem in Favorites"
-echo "Show Hidden Files"
-echo "にチェックを入れてください。"
+echo "c.NotebookApp.password = 'sha1:〜〜〜〜〜〜'"
